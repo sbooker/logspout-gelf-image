1711f746b07df93056500143626759115ad31ef3
Sergey Knigin <sergey.knigin.dev@gmail.com>
2022-11-27 11:08:56 +0300

    Use v.1.1.2

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 apk update
 apk add --no-cache --update go build-base git mercurial ca-certificates pcre2-dev
 cd /src
-go get github.com/sbooker/logspout-gelf@1.1.1
+go get github.com/sbooker/logspout-gelf@1.1.2
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /root/go /var/cache/apk/*
